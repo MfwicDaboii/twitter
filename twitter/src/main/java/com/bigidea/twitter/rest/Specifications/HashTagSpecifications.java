817275a525332edbe09603cda95e02dfb0549aac
joshua <joshua.keulers@gmail.com>
2020-01-11 13:58:05 +0100

    updated specifications

--- twitter/src/main/java/com/bigidea/twitter/rest/Specifications/HashTagSpecifications.java
+++ twitter/src/main/java/com/bigidea/twitter/rest/Specifications/HashTagSpecifications.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public class HashTagSpecifications {
 
     //Dit is een query voor het zoeken in de db op basis van de naam van een hashtag entiteit
-    public static Specification<HashTagEntity>getBy(String name){
+    public static Specification<HashTagEntity>nameLike(String name){
         return (entity,cq,cb) -> Optional.ofNullable(name).map(n -> cb.like(entity.get("name"),"%" + n + "%")).orElse(null);
     }
 }
